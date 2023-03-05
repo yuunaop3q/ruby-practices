12ed5d39b644d5f57c5bc5602b21d1c40eafb188
yuunao <yu.chijiwa@gmail.com>
2023-03-05 16:25:02 +0900

    PRのコメントに対しての再修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -12,19 +12,17 @@ params = opt.getopts('m:', 'y:')
 
 # コマンドライン引数が設定されていない時とされている時の年月の表示
 
-if params['m'].nil? && params ['y'].nil?
-  month = today.mon
-  year = today.year
-elsif params['m'].nil? && params ['y']
-  month = today.month
-  year = params['y'].to_i
-elsif params['m'] && params['y'].nil?
-  month = params['m'].to_i
-  year = today.year
-else
-  month = params['m'].to_i
-  year = params['y'].to_i
-end
+month = if params['m'].nil?
+          today.month
+        else
+          params['m'].to_i
+        end
+
+year = if params['y'].nil?
+         today.year
+       else
+         params['y'].to_i
+       end
 
 first_day = Date.new(year, month, 1)
 last_day = Date.new(year, month, -1)
@@ -32,19 +30,19 @@ print "       #{month}月#{year}\n"
 # 曜日の表示
 
 day_of_weeks = %w[日 月 火 水 木 金 土]
-day_of_weeks.each { |d| print d, ' ' }
-puts
+puts day_of_weeks.join(' ')
+
 #  1行目の空白（初日が何曜日始まりかを表す）のコード
 
 print '   ' * first_day.wday
 
 # 月末までの数字を表すコード
 
-(first_day.day..last_day.day).each do |number|
-  if number < 10
-    print(" #{number} ")
+(first_day.day..last_day.day).each do |months|
+  if months < 10
+    print(" #{months} ")
   else
-    print("#{number} ")
+    print("#{months} ")
   end
-  print "\n" if Date.new(year, month, number).wday == 6
+  print "\n" if Date.new(year, month, months).saturday?
 end
