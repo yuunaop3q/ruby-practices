880a72c57c897e531b29aba6752a02849296060c
yuunao <yu.chijiwa@gmail.com>
2023-05-08 14:10:59 +0900

    sortをtake_files内で行い、sort_filesの名前の変更、transposed_arrayの変更

--- 04.ls/ls1.rb
+++ 04.ls/ls1.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
 def take_files
-  Dir.glob('*')
+  Dir.glob('*').sort
 end
 
 def print_files_in_columns(col_size)
-  sort_files = take_files.sort
-  maxlen = sort_files.max_by(&:length).length
-  lines = (sort_files.size / col_size.to_f).ceil
-  sort_files << nil.to_s until (sort_files.size % col_size).zero?
+  use_files = take_files
+  maxlen = use_files.max_by(&:length).length
+  lines = (use_files.size / col_size.to_f).ceil
+  use_files << nil.to_s until (use_files.size % col_size).zero?
 
-  sort_array = sort_files.each_slice(lines).to_a
-
-  transposed_array = sort_array.transpose
+  transposed_array = use_files.each_slice(lines).to_a.transpose
 
   transposed_array.each do |file|
     puts file.map { |f| f.ljust(maxlen + 10) }.join
