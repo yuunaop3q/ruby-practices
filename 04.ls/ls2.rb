ba9b67c31c0946ea46ee5f9398836a79beb7c21d
yuunao <yu.chijiwa@gmail.com>
2023-05-20 14:32:40 +0900

    lsコマンド2の課題提出

---
+++ 04.ls/ls2.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+def take_files
+  Dir.glob('*').sort
+end
+
+def take_dot_files
+  Dir.glob('*', File::FNM_DOTMATCH)
+end
+
+def print_files_in_columns(col_size)
+  use_files = take_files
+  maxlen = use_files.max_by(&:length).length
+  lines = calculate_number_of_rows(use_files, col_size)
+  use_files = fill_empty_files(use_files, lines, col_size)
+  display_files_in_columns(transpose_files(use_files, lines), maxlen)
+end
+
+def print_dot_files_in_columns(col_size)
+  use_files = take_dot_files
+  maxlen = use_files.max_by(&:length).length
+  lines = calculate_number_of_rows(use_files, col_size)
+  use_files = fill_empty_files(use_files, lines, col_size)
+  display_files_in_columns(transpose_files(use_files, lines), maxlen)
+end
+
+def calculate_number_of_rows(file_count, col_size)
+  (file_count.size / col_size.to_f).ceil
+end
+
+def fill_empty_files(files, lines, col_size)
+  extra_files = lines * col_size - files.size
+  files.concat([nil] * extra_files)
+end
+
+def transpose_files(files, lines)
+  files.each_slice(lines).to_a.transpose
+end
+
+def display_files_in_columns(transposed_array, maxlen)
+  transposed_array.each do |file|
+    puts file.map { |f| f.to_s.ljust(maxlen + 10) }.join
+  end
+end
+
+if ARGV.include?('-a')
+  print_dot_files_in_columns(3)
+else
+  print_files_in_columns(3)
+end
