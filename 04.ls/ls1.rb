125fd98fdc8ee8a999dd2a3a85aa52d5bd6c8c52
yuunao <yu.chijiwa@gmail.com>
2023-07-06 14:12:44 +0900

    前回までのプラクティスを使用する形に変更

--- 04.ls/ls1.rb
+++ 04.ls/ls1.rb
@@ -1,6 +1,42 @@
 # frozen_string_literal: true
 
 require 'etc'
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.on('-l') do
+    options[:info] = true
+  end
+end.parse!
+
+def print_files_in_columns(col_size)
+  use_files = take_files
+  maxlen = use_files.max_by(&:length).length
+  lines = calculate_number_of_rows(use_files, col_size)
+  use_files = fill_empty_files(use_files, lines, col_size)
+  display_files_in_columns(transpose_files(use_files, lines), maxlen)
+end
+
+def calculate_number_of_rows(file_count, col_size)
+  (file_count.size / col_size.to_f).ceil
+end
+
+def fill_empty_files(files, lines, col_size)
+  extra_files = lines * col_size - files.size
+  files.concat([nil] * extra_files)
+end
+
+def transpose_files(files, lines)
+  files.each_slice(lines).to_a.transpose
+end
+
+def display_files_in_columns(transposed_array, maxlen)
+  transposed_array.each do |file|
+    arrange_files = file.compact.map { |f| f.to_s.ljust(maxlen + 10) }
+    puts arrange_files.join
+  end
+end
 
 def first_hash_symbols(align_permissions)
   align_permissions.gsub(
@@ -28,81 +64,88 @@ def hash_symbols
   }
 end
 
-def get_file_info(file_path)
+def file_info(file_path)
   file_stat = File::Stat.new(file_path)
-  permissions = get_permissions(file_stat)
-  hard_links = get_hard_links(file_stat)
-  user_name = get_user_name(file_stat)
-  group_name = get_group_name(file_stat)
-  size = get_size(file_stat)
-  modified_time = get_modified_time(file_stat)
-  file_name = get_file_name(file_path)
+  permissions = permissions(file_stat)
+  hard_links = hard_links(file_stat)
+  user_name = user_name(file_stat)
+  group_name = group_name(file_stat)
+  size = size(file_stat)
+  modified_time = modified_time(file_stat)
+  file_name = file_name(file_path)
   "#{permissions} #{hard_links} #{user_name} #{group_name} #{size} #{modified_time} #{file_name}"
 end
 
-def files_list(directory_path)
-  Dir.entries(directory_path) - ['.', '..']
+def take_files
+  Dir.glob('*').sort
 end
 
-def get_permissions(file_stat)
+def permissions(file_stat)
   perm = file_stat.mode.to_s(8)
-  format('%06d', perm)
+  format('%06o', perm)
   permissions_replacement(file_stat)
 end
 
-def get_hard_links(file_stat)
+def hard_links(file_stat)
   file_stat.nlink.to_s.rjust(2)
 end
 
-def get_user_name(file_stat)
+def user_name(file_stat)
   user_id = file_stat.uid
   Etc.getpwuid(user_id).name.ljust(8)
 end
 
-def get_group_name(file_stat)
+def group_name(file_stat)
   group_id = file_stat.gid
   Etc.getgrgid(group_id).name.ljust(8)
 end
 
-def get_size(file_stat)
+def size(file_stat)
   file_stat.size.to_s.rjust(8)
 end
 
-def get_modified_time(file_stat)
+def modified_time(file_stat)
   file_stat.ctime.strftime('%m %d %H:%M')
 end
 
-def get_file_name(file_path)
+def file_name(file_path)
   File.basename(file_path)
 end
 
 def permissions_replacement(file_stat)
-  perm = file_stat.mode.to_s(8)
-  align_permissions = format('%06d', perm)
-  captured_strings = []
+  align_permissions = format('%06o', file_stat.mode)
   first_permissions = first_hash_symbols(align_permissions)
-  captured_strings << first_permissions.match(/(p|c|d|b|-|l|s)/)
-  specifying_permissions(captured_strings, first_permissions, hash_symbols)
-  captured_strings.join('')
+  [
+    first_hash_symbols(align_permissions).match(/(p|c|d|b|-|l|s)/),
+    specifying_permissions(first_permissions, hash_symbols) # permissionの文字列を返すように修正する
+  ].join
 end
 
-def specifying_permissions(captured_strings, first_permissions, hash_symbols)
-  captured_strings << first_permissions[2..].chars.map { |digit| hash_symbols[digit] }
+def specifying_permissions(first_permissions, hash_symbols)
+  first_permissions[2..].chars.map { |digit| hash_symbols[digit] }
 end
 
-def list_directory(directory_path, files_list)
+def path_info(take_files, directory_path)
   files_info = []
-  total_size = 0
-  files_list.each do |file|
-    next if file.start_with?('.')
-
+  take_files.map do |file|
     file_path = File.join(directory_path, file)
-    files_info << get_file_info(file_path)
-    total_size += File.size(file_path)
+    files_info << file_info(file_path)
   end
-  puts "total #{total_size}"
-  puts files_info.sort
+  files_info
+end
+
+def list_directory(directory_path, take_files, options, col_size)
+  if options[:info] == true
+    total_file_blocks = take_files.sum { |file| File.stat(file).blocks }
+    puts "total #{total_file_blocks}"
+    files_info = path_info(take_files, directory_path)
+    puts files_info
+  else
+    col_size = 3
+    print_files_in_columns(col_size)
+  end
 end
 
 directory_path = '/Users/chi/ruby-practices/04.ls'
-list_directory(directory_path, files_list(directory_path))
+col_size = 3
+list_directory(directory_path, take_files, options, col_size)
